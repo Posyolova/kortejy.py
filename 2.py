88a904e2864d4b95e902a699f9afefbda517f34e
Posyolova <143779118+Posyolova@users.noreply.github.com>
2023-12-17 20:21:13 +0700

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,14 +1,16 @@
-A = 0
-B = -1
-def slicer(w, q):
-    fir = sec = A   
-    if q in w:
-        fir = w.index(q)
-    if w.count(q) > 1:
-        sec = w.index(q, fir + 1) + 1
-    else:
-        sec = B
-    return w[fir:sec]
-print(slicer((1, 2, 3), 8))
-print(slicer((1, 8, 3, 4, 8, 8, 9, 2), 8))
-print(slicer((1, 2, 8, 5, 1, 2, 9), 8))
+def slicer(tpl,element):
+    #условие, которое проверяет есть ли элемент кортежа
+    if element not in tpl:
+        return()
+        #кол-во вхождения элемента в кортеж
+    if tpl.count(element)== 1:
+        #функция возвращает отрезок кортежа начиная со вхождения элемента и до конца
+        return (tpl[tpl.index(element):])
+    elif tpl.count (element)==2:
+        s = ''
+        #цикл, который перебирает кортеж
+        for i in tpl:
+            #перенос содержания кортежа в строку s
+           s += str(i)
+            #возвращение отрезка кортежа начиная с 1 вхождения элемента заканчивая 2
+        return tpl[s.index(str(element)):s.index(str(element))+1]
