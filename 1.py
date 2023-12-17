05b62318eba327d031bdd7df78029ae921cc02e8
Posyolova <143779118+Posyolova@users.noreply.github.com>
2023-12-17 18:26:37 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 def tpl_sort(tpl):
     for element in tpl:
-        if not isinstance(element, int):
-            return ()
-    return tuple(sorted(tpl))
+        if not isinstance(element, int): # проверяет является ли целым числом
+            return (tpl) # если нет то в исходный 
+    return tuple(sorted(tpl)) # отсортированный кортеж
 print(tpl_sort((4, 8, 3, 1, 9)))
 print(tpl_sort((4, 6, 2.1, 1, 7)))
